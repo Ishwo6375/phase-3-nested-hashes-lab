d8e7a1fd3a5e2ae619337ff250edcf3643b18ce3
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-07-05 20:05:32 -0400

    completed assignment

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,6 +15,7 @@ def hopper
       }
     }
 
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
       }
     }
 
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +57,7 @@ def dennis_ritchies_language
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +84,17 @@ def adding_matz
       }
     }
 
+    programmer_hash[:matz] = {
+      known_for: "Ruby",
+      languages: ["LISP", "C"]
+    }
+
+    programmer_hash[:yukihiro_matsumoto] = {
+      known_for: "Ruby",
+      languages: ["LISP", "C"]
+    }
+
+    programmer_hash
 end
 
 def changing_alan
@@ -104,6 +117,9 @@ def changing_alan
       }
     }
 
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +142,7 @@ def adding_to_dennis
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+    programmer_hash
 end
